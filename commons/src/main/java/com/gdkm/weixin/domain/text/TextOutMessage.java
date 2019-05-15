6d9808f65866aa0fb7100070acc48302d5b1c437
mxdlance <48517626+mxdlance@users.noreply.github.com>
2019-05-15 19:16:50 +0800

    调整TextOutMessage

--- commons/src/main/java/com/gdkm/weixin/domain/text/TextOutMessage.java
+++ commons/src/main/java/com/gdkm/weixin/domain/text/TextOutMessage.java
@@ -3,20 +3,35 @@ package com.gdkm.weixin.domain.text;
 import com.gdkm.weixin.domain.OutMessage;
 
 public class TextOutMessage extends OutMessage {
-	private String text;
+	private TextContent text;
 
 	public TextOutMessage(String toUser, String text) {
 		super(toUser, "text");
-		this.text = text;
-		// TODO Auto-generated constructor stub
+		this.text = new TextContent(text);
 	}
 
-	public String getText() {
+	public TextContent getText() {
 		return text;
 	}
 
-	public void setText(String text) {
+	public void setText(TextContent text) {
 		this.text = text;
+	}
+
+	public static class TextContent {
+		private String content;
+
+		public TextContent(String content) {
+			this.content = content;
+		}
+
+		public String getContent() {
+			return content;
+		}
+
+		public void setContent(String content) {
+			this.content = content;
+		}
 	}
 
 }
